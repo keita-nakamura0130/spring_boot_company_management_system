8f3a204244904910d65a0944c8ecb75c6281467d
Keita Nakamura <bzhtadgm903@yahoo.co.jp>
2020-12-31 16:59:50 +0900

    first commit

---
+++ src/main/java/com/keita/nakamura/SpringBootCompanyManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.keita.nakamura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCompanyManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCompanyManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keita/nakamura/SpringBootCompanyManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keita.nakamura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCompanyManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
