1d9725804398f651c9adafb551555e49b25ecd9c
Keita Nakamura <bzhtadgm903@yahoo.co.jp>
2021-01-05 00:16:58 +0900

    追加：都道府県リスト

--- src/main/java/com/keita/nakamura/controller/CompaniesController.java
+++ src/main/java/com/keita/nakamura/controller/CompaniesController.java
@@ -12,10 +12,18 @@ import com.keita.nakamura.mapper.CompanyMapper;
 
 @Controller
 public class CompaniesController {
-    
+
     @Autowired
     CompanyMapper CompanyMapper;
-    
+
+    /**
+     * 都道府県リスト(都道府県コードを都道府県に変換するために使用)
+     */
+    final String[] PREFECTURES = { null, "北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県",
+            "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府",
+            "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県",
+            "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県" };
+
     /**
      * 会社一覧
      */
@@ -23,7 +31,9 @@ public class CompaniesController {
     public String index(Model model) {
         List<Company> companies = CompanyMapper.findAll();
         model.addAttribute("companies", companies);
-        
+
+        model.addAttribute("PREFECTURES", this.PREFECTURES);
+
         return "/companies/index";
     }
 }
