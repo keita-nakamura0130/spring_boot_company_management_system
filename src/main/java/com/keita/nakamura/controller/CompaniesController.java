4c4c65fbca4199a5204dd44803750fa02e8e846c
Keita Nakamura <bzhtadgm903@yahoo.co.jp>
2021-01-09 19:32:25 +0900

    追加：会社詳細

--- src/main/java/com/keita/nakamura/controller/CompaniesController.java
+++ src/main/java/com/keita/nakamura/controller/CompaniesController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.keita.nakamura.entity.Company;
@@ -38,5 +39,18 @@ public class CompaniesController {
         model.addAttribute("PREFECTURES", PREFECTURES);
 
         return "/companies/index";
+    }
+
+    /**
+     * 会社詳細
+     */
+    @RequestMapping(value = "/companies/show/{id}")
+    public String show(@PathVariable int id, Model model) {
+        Company company = CompanyMapper.findById(id);
+        model.addAttribute("company", company);
+
+        model.addAttribute("PREFECTURES", PREFECTURES);
+
+        return "companies/show";
     }
 }
