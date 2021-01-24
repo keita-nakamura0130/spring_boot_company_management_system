4737537ea670dfd26598cc05320c65b423a6d86d
Keita Nakamura <bzhtadgm903@yahoo.co.jp>
2021-01-24 22:10:27 +0900

    改善：Userサービスをリファクタリング

--- src/main/java/com/keita/nakamura/service/UserService.java
+++ src/main/java/com/keita/nakamura/service/UserService.java
@@ -1,27 +1,18 @@
 package com.keita.nakamura.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.keita.nakamura.entity.User;
 import com.keita.nakamura.mapper.UserMapper;
 
 /**
- * Companyサービス
+ * Userサービス
  */
 @Service
-public class UserService implements UserDetailsService {
+public class UserService  {
     @Autowired
-    UserMapper UserMapper;
+    UserMapper userMapper;
 
     /**
      * ユーザー名よりをユーザーを1件取得
@@ -30,7 +21,7 @@ public class UserService implements UserDetailsService {
      * @return
      */
     public User findByName(String name) {
-        return UserMapper.findByName(name);
+        return userMapper.findByName(name);
     }
     
     /**
@@ -39,25 +30,6 @@ public class UserService implements UserDetailsService {
      * @param user
      */
     public void insert(User user) {
-        UserMapper.insert(user);
-    }
-    
-    @Override
-    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
-        User user = UserMapper.findByName(name);
-        System.out.println("LOGINUSER INSTANCE");
- 
-        if (user == null) {
-            throw new UsernameNotFoundException("userName" + name + "was not found in the database");
-        }
- 
-        List<GrantedAuthority> grantList = new ArrayList<GrantedAuthority>();
-        GrantedAuthority authority = new SimpleGrantedAuthority("USER");
-        grantList.add(authority);
- 
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
- 
-        UserDetails userDetails = (UserDetails) new User(user.getName(), encoder.encode(user.getPassword()), grantList);
-        return userDetails;
+        userMapper.insert(user);
     }
 }
