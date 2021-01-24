48160238a473e05e31f9348ec2ca8380506d6aa6
Keita Nakamura <bzhtadgm903@yahoo.co.jp>
2021-01-24 21:42:09 +0900

    改善：Userエンティティをリファクタリング
    ・使用されていないコンストラクタを削除。

--- src/main/java/com/keita/nakamura/entity/User.java
+++ src/main/java/com/keita/nakamura/entity/User.java
@@ -1,9 +1,5 @@
 package com.keita.nakamura.entity;
 
-import java.util.List;
-
-import org.springframework.security.core.GrantedAuthority;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,24 +9,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class User {
-
-    /**
-     * 引数なしコンストラクタ
-     */
-    public User() {
-
-    }
-
-    /**
-     * コンストラクタ
-     *
-     * @param name
-     * @param encode
-     * @param grantList
-     */
-    public User(String name, String encode, List<GrantedAuthority> grantList) {
-
-    }
 
     /**
      * ID
