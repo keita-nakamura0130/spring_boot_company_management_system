c98d8369be4311b399a16cc3022b3b4d39c7fe05
Keita Nakamura <bzhtadgm903@yahoo.co.jp>
2021-01-24 21:53:30 +0900

    改善：LoginUserDetails

--- src/main/java/com/keita/nakamura/security/LoginUserDetails.java
+++ src/main/java/com/keita/nakamura/security/LoginUserDetails.java
@@ -6,20 +6,31 @@ import org.springframework.security.core.userdetails.User;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+/**
+ * LoginUserDetailsクラス
+ */
 @Data
 //equals()とhashCode()を生成するが親クラスのメソッドは呼び出さない
 @EqualsAndHashCode(callSuper = false)
 public class LoginUserDetails extends User {
+
     /**
-     * 
+     * serialVersionUID
      */
     private static final long serialVersionUID = 1L;
-    //employeeテーブルから取得したオブジェクトを格納
+
+    /**
+     * Userエンティティ
+     */
     private final com.keita.nakamura.entity.User user;
 
-    //認証処理
+    /**
+     * 認証処理コンストラクタ
+     *
+     * @param user
+     * @param role
+     */
     public LoginUserDetails(com.keita.nakamura.entity.User user, String role) {
-        //employeeテーブルの名前とパスワードでログイン認証を行う
         super(user.getName(), user.getPassword(), AuthorityUtils.createAuthorityList(role));
         this.user = user;
     }
