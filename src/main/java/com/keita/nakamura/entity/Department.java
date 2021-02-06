e7d977efddee32cf2ff0a884a99ba6741e669875
Keita Nakamura <bzhtadgm903@yahoo.co.jp>
2021-02-06 23:06:04 +0900

    更新：Entity

--- src/main/java/com/keita/nakamura/entity/Department.java
+++ src/main/java/com/keita/nakamura/entity/Department.java
@@ -1,5 +1,41 @@
 package com.keita.nakamura.entity;
 
-public class Department {
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Departmentクラス
+ */
+@Getter
+@Setter
+public class Department {
+    /**
+     * ID
+     */
+    private int id;
+    
+    /**
+     * 役職名
+     */
+    @NotBlank(message = "必須入力です")
+    @Size(max = 20, message="20文字以内で入力してください")
+    private String name;
+    
+    /**
+     * 作成日時
+     */
+    private String created;
+    
+    /**
+     * 更新日時
+     */
+    private String modified;
+    
+    /**
+     * 削除日時
+     */
+    private String deleted;
 }
